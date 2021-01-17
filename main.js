7db9b952505d77c3f2ab576a149a491b1ef0b6fc
George Di Iorio <diioriodesigns@gmail.com>
2021-01-17 07:44:13 -0800

    Updated html and css and made connection with Firebase.

--- main.js
+++ main.js
@@ -2,16 +2,17 @@
 var firebaseConfig = {
     apiKey: "AIzaSyA3fRTBNDV39mkelf9zb-4AgwXKp5HJOzg",
     authDomain: "contact-form-8ae19.firebaseapp.com",
-    databaseURL:"https://contact-form-8ae19-default-rtdb.firebaseio.com/",
+    databaseURL: "https://contact-form-8ae19-default-rtdb.firebaseio.com",
     projectId: "contact-form-8ae19",
     storageBucket: "contact-form-8ae19.appspot.com",
     messagingSenderId: "497820250688",
-    appId: "1:497820250688:web:a35f4fb4eea2c2823265a5"
+    appId: "1:497820250688:web:732b711b392e756c3265a5"
   };
+  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
 // REFERENCE MESSAGES IN DATABASE
-//let messagesRef = firebase.database().ref('messages');
+let messagesRef = firebase.database().ref('messages');
 
 // LISTEN FOR FORM SUBMIT
 document.getElementById('contactForm').addEventListener('submit', submitForm);
@@ -30,7 +31,16 @@ function submitForm(e){
     //SAVE MESSAGE
     saveMessage(name, company, email, phone, message);
 
+//SHOW ALERT
+document.querySelector('.alert').style.display = 'block';
 
+//HIDE ALERT AFTER 3 SECONDS
+setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+}, 3000);
+
+//CLEAR FORM
+document.getElementById('contactForm').reset();
 }
 
 //FUNCTION TO GET FROM VALUES
